bc9ea795710665567b9779497c6c781757150ea4
rhit-mahonslg <mahonslg@rose-hulman.edu>
2026-05-03 20:27:47 -0400

    UpdatePlayer

--- CSSE220-Final-Project-Starter-main/CSSE220-Final-Project-Starter-main/src/model/Player.java
+++ CSSE220-Final-Project-Starter-main/CSSE220-Final-Project-Starter-main/src/model/Player.java
@@ -1,15 +1,25 @@
 package model;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Player {
 	private int row;
 	private int col;
 	private static final int TILE_SIZE = 40;
+	BufferedImage sprite;
 	
 	public Player(int row, int col) {
 		this.row = row;
 		this.col = col;
+		try {
+			sprite = ImageIO.read(Player.class.getResource("tennis.png"));
+		} catch (IOException | IllegalArgumentException e) {
+			sprite = null;
+		}
 	}
 
 	public int getRow() {
